5dd8ae37fea4e39a35f7925156547fe94169a300
hiruboy1 <hirunvikasitha808@gmail.com>
2025-07-25 14:54:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '',
+    SESSION_ID: process.env.SESSION_ID || 'GENUX-7b7283',
     PREFIX: process.env.PREFIX || '.',
     OWNER_NUMBER: process.env.OWNER_NUMBER || ['94771029271'],
     MONGODB_URI: process.env.MONGODB_URI || '',
